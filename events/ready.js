9dd103d9a2a44fbf5701ae5973fda391fbc79b13
AlienFreek <scottlask01@gmail.com>
2023-02-09 18:41:37 -0500

    Now sets presence on ready

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,12 @@
+const {ActivityType} = require("discord.js");
 module.exports = {
     name: 'ready',
     once: true,
     execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
+        client.user.setPresence({
+            activities: [{ name: `/info`, type: ActivityType.Playing }],
+            status: 'online',
+        });
     },
 };
